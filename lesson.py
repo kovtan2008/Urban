81f6b158a99ebf9abe69b6bcf59b16b705cf5f93
kovtan <63555281+kovtan2008@users.noreply.github.com>
2024-07-24 00:03:47 +0300

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,31 @@
+print(12+89-1)
+
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program opt1
+print(1234 // 10 % 100)
+print(5678 // 10 % 100)
+print(23+67)
+
+# 3rd program opt2
+print(int(1234 / 10 % 100))
+print(int(5678 / 10 % 100))
+print((23+67))
+
+# 3rd program opt3
+a = [1234 // 10 % 100]
+b = [5678 // 10 % 100]
+total = sum(a+b)
+print(total)
+
+# 4th program opt1
+print(13.42 == 42.13 or 13 == 42 or 13 == 13 or 42 == 42 or 42 == 13)
+
+# 4th program opt2
+print(int(13.42))
+print(int((42.13 - 42) * 100))
+print(13 == 13)
