ab715d3023aee0d5a0026437a4d0918cf1227348
kovtan <63555281+kovtan2008@users.noreply.github.com>
2024-08-20 01:14:41 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,17 @@
+#  Организация программы
+my_string = input("Хочешь узнать сколько в этом тексте символов? ")
+count = len("Хочешь узнать сколько в этом тексте символов?")
+if my_string.lower() != "да":
+            print('Ну, как хочешь, тебе же хуже...')
+            quit()
+if my_string.lower() != "нет":
+    print("Хорошо, в том тексте было", count, "символа")
+
+# Работа с методами строк
+
+my_string = "Хочешь узнать сколько в этом тексте символов?"
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
